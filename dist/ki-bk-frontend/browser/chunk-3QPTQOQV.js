b43505a6d33f8ef298f9bfdbeb737f15e8d5023f
Tadah <dimetric.tadah@mobilehup.com>
2024-11-27 13:57:51 +0100

    remove dit folder

--- dist/ki-bk-frontend/browser/chunk-3QPTQOQV.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-GGMUMUQ3.js";import"./chunk-OP6G6OKL.js";import"./chunk-BMO5JKBW.js";export{a as FeaturesModule};
--- dist/ki-bk-frontend/browser/chunk-GBJNX5WT.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-OP6G6OKL.js";import"./chunk-BMO5JKBW.js";export{a as HomeModule};
